aafa2479701c358d1378ec9318071ed912f6b3d0
jjriosjose <168056859+jjriosjose@users.noreply.github.com>
2026-08-24 00:22:42 -0400

    feat: cargar estilos globales V0.6.2

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import './styles.css'
 import './styles/v058.css'
+import './styles/v062.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AuthProvider><App/></AuthProvider></React.StrictMode>)
 if('serviceWorker'in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}))}
