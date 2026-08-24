b18935eff2556393534f317f13993d5fe5008501
jjriosjose <168056859+jjriosjose@users.noreply.github.com>
2026-08-24 01:01:17 -0400

    style: cargar estilos v0.6.3

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,6 @@ import { AuthProvider } from './context/AuthContext'
 import './styles.css'
 import './styles/v058.css'
 import './styles/v062.css'
+import './styles/v063.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AuthProvider><App/></AuthProvider></React.StrictMode>)
 if('serviceWorker'in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}))}
