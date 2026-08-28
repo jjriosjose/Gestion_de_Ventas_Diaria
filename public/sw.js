c7989c2de68a4b628262530a3a713013d6450789
jjriosjose <168056859+jjriosjose@users.noreply.github.com>
2026-08-27 21:52:50 -0400

    chore: rotate shell cache for beta.10

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='gvd-shell-v065-beta9';
+const CACHE='gvd-shell-v065-beta10';
 const SHELL=['/manifest.webmanifest','/pwa-icon.svg','/pwa-maskable.svg','/logo-karaka.png'];
 
 self.addEventListener('install',event=>{
