1c4c898f4529c513162873793483f0cfc527ccf7
jjriosjose <168056859+jjriosjose@users.noreply.github.com>
2026-09-06 14:41:08 -0400

    Logistics V1: route POD history

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { Coverage } from './pages/Coverage'
 import { Logistics } from './pages/Logistics'
 import { DeliveryAccessAdmin } from './pages/DeliveryAccessAdmin'
 import { DeliveryIncidents } from './pages/DeliveryIncidents'
+import { DeliveryHistory } from './pages/DeliveryHistory'
 import { ExternalDelivery } from './pages/ExternalDelivery'
 import { ReportsV2 } from './pages/ReportsV2'
 import { Admin } from './pages/Admin'
@@ -55,6 +56,7 @@ function Protected(){
     <Route path="agenda" element={<RequirePermission permission="agenda.view"><Agenda/></RequirePermission>}/>
     <Route path="recepcion" element={<RequirePermission permission="reception.view"><Reception/></RequirePermission>}/>
     <Route path="logistica" element={<RequirePermission permission="logistics.view"><Logistics/></RequirePermission>}/>
+    <Route path="logistica/historial" element={<RequirePermission permission="logistics.view"><DeliveryHistory/></RequirePermission>}/>
     <Route path="logistica/accesos" element={<RequirePermission permission="logistics.manage"><DeliveryAccessAdmin/></RequirePermission>}/>
     <Route path="logistica/incidencias" element={<RequirePermission permission="logistics.tracking"><DeliveryIncidents/></RequirePermission>}/>
     <Route path="reportes" element={<RequirePermission permission="reports.view"><ReportsV2/></RequirePermission>}/>
